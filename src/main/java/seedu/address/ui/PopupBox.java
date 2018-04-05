4eac5b268276eb54cace6e432000c3100bd41544
fishTT <1070450490@qq.com>
2018-04-06 01:39:51 +0800

    change PopupBox to javaFX

--- src/main/java/seedu/address/ui/PopupBox.java
+++ src/main/java/seedu/address/ui/PopupBox.java
@@ -1,70 +1,33 @@
 package seedu.address.ui;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
+import org.controlsfx.control.Notifications;
 
+import javafx.geometry.Pos;
 
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 
 /**
- * An UI component that displays information of a {@code Popup}.
+ * Panel containing the list of books.
  */
-
-class PopupBox {
-    PopupBox() {
-        String message = "You got a new notification message. Isn't it awesome to have such a notification message.";
-        String header = "This is header of notification message";
-        JFrame frame = new JFrame();
-        frame.setSize(300, 125);
-        frame.setUndecorated(true);
-        Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize(); // size of the screen
-        Insets toolHeight = Toolkit.getDefaultToolkit().getScreenInsets(frame.getGraphicsConfiguration());
-        frame.setLocation(scrSize.width - frame.getWidth(), scrSize.height - toolHeight.bottom - frame.getHeight());
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.weightx = 1.0f;
-        constraints.weighty = 1.0f;
-        constraints.insets = new Insets(5, 5, 5, 5);
-        constraints.fill = GridBagConstraints.BOTH;
-        JLabel headingLabel = new JLabel(header);
-        //headingLabel .setIcon(); // --- use image icon you want to be as heading image.
-        headingLabel.setOpaque(false);
-        frame.add(headingLabel, constraints);
-        constraints.gridx++;
-        constraints.weightx = 0f;
-        constraints.weighty = 0f;
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.anchor = GridBagConstraints.NORTH;
-        JButton clonesButton = new JButton(new AbstractAction("X") {
-            @Override
-                public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-            }
-        });
-        clonesButton.setMargin(new Insets(1, 4, 1, 4));
-        clonesButton.setFocusable(false);
-        frame.add(clonesButton, constraints);
-        constraints.gridx = 0;
-        constraints.gridy++;
-        constraints.weightx = 1.0f;
-        constraints.weighty = 1.0f;
-        constraints.insets = new Insets(5, 5, 5, 5);
-        constraints.fill = GridBagConstraints.BOTH;
-        JLabel messageLabel = new JLabel("<HtMl>" + message);
-        frame.add(messageLabel, constraints);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+public class PopupBox extends UiPart<Region> {
+    private static final String FXML = "PopupBox.fxml";
+    private static final String MESSAGE = "There is no end to education. It is not that you read a book,\n"
+            + " pass an examination, and finish with education.\n"
+            + "The whole of life, from the moment you are born to\n "
+            + "moment you die, is a process of learning.\n";
+    public PopupBox() {
+        super(FXML);
+        Notifications pop = Notifications.create()
+                .title("Quote of the day")
+                .text(MESSAGE)
+                .graphic(null)
+                .hideAfter(Duration.seconds(10))
+                .position(Pos.BOTTOM_RIGHT);
+        pop.show();
     }
+
+
 }

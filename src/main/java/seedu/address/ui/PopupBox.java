bacaae9494db15def80e9e075ce800146a465097
fishTT <1070450490@qq.com>
2018-04-08 17:32:32 +0800

    Enable PopupBox to read java

--- src/main/java/seedu/address/ui/PopupBox.java
+++ src/main/java/seedu/address/ui/PopupBox.java
@@ -1,5 +1,14 @@
 package seedu.address.ui;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 import org.controlsfx.control.Notifications;
 
 import javafx.geometry.Pos;
@@ -7,18 +16,37 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.Region;
 import javafx.util.Duration;
 
-
+//@@author fishTT
 /**
  * Panel containing the list of books.
  */
 public class PopupBox extends UiPart<Region> {
     private static final String FXML = "PopupBox.fxml";
-    private static final String MESSAGE = "There is no end to education. It is not that you read a book,\n"
-            + " pass an examination, and finish with education.\n"
-            + "The whole of life, from the moment you are born to\n "
-            + "moment you die, is a process of learning.\n";
-    public PopupBox() {
+    private static String MESSAGE ="";
+
+
+    public String getQuote() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/text/quote.txt"));
+        List<String> lines = new ArrayList<String>();
+
+        String line = reader.readLine();
+
+        while( line != null ) {
+            lines.add(line);
+            line = reader.readLine();
+        }
+
+// Choose a random one from the list
+        Random r = new Random();
+        String randomString = lines.get(r.nextInt(lines.size()));
+        return randomString;
+    }
+
+
+
+    public PopupBox() throws IOException {
         super(FXML);
+        MESSAGE = getQuote();
         Notifications pop = Notifications.create()
                 .title("Quote of the day")
                 .text(MESSAGE)

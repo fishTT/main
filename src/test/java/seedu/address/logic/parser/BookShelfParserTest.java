c91b051380f73e9df4a3f61e864db4692b56941b
fishTT <1070450490@qq.com>
2018-04-14 20:53:55 +0800

    minor adjustment to code

--- src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
@@ -50,30 +50,6 @@ public class BookShelfParserTest {
         new BookShelfParser(null);
     }
 
-    //@@author fishTT
-    @Test
-    public void caseInsensitive_success() throws Exception {
-        /* Case: firstCharUppercase_success() -> added */
-        char[] commandWord = ListCommand.COMMAND_WORD.toCharArray();
-        commandWord[0] = Character.toUpperCase(commandWord[0]);
-        String firstCharUppercaseCommand = String.copyValueOf(commandWord);
-        assertTrue(parser.parseCommand(firstCharUppercaseCommand) instanceof ListCommand);
-
-        /* Case: lastCharUppercase_success() -> added */
-        commandWord[commandWord.length - 1] = Character.toUpperCase(commandWord[commandWord.length - 1]);
-        String lastCharUppercaseCommand = String.copyValueOf(commandWord);
-        assertTrue(parser.parseCommand(lastCharUppercaseCommand) instanceof ListCommand);
-
-        /* Case: middleCharUppercase_success() -> added */
-        commandWord[commandWord.length / 2] = Character.toUpperCase(commandWord[commandWord.length / 2]);
-        String middleCharUppercaseCommand = String.copyValueOf(commandWord);
-        assertTrue(parser.parseCommand(middleCharUppercaseCommand) instanceof ListCommand);
-
-        /* Case: AllCharUppercase_success() -> added */
-        String allCharUppercaseCommand = ListCommand.COMMAND_WORD.toUpperCase();
-        assertTrue(parser.parseCommand(allCharUppercaseCommand) instanceof ListCommand);
-    }
-
     @Test
     public void applyCommandAlias_matchingAlias_success() throws Exception {
         String namedArgs = "s/unread by/title";
@@ -179,6 +155,31 @@ public class BookShelfParserTest {
         assertTrue(parser.parseCommand(UndoCommand.COMMAND_WORD) instanceof UndoCommand);
         assertTrue(parser.parseCommand("undo 3") instanceof UndoCommand);
     }
+
+    //@@author fishTT
+    @Test
+    public void parseCommand_mixedCaseCommandWord_success() throws Exception {
+        /* Case: first character is uppercase */
+        char[] commandWord = ListCommand.COMMAND_WORD.toCharArray();
+        commandWord[0] = Character.toUpperCase(commandWord[0]);
+        String firstCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(firstCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: last character is uppercase */
+        commandWord[commandWord.length - 1] = Character.toUpperCase(commandWord[commandWord.length - 1]);
+        String lastCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(lastCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: middle character is uppercase */
+        commandWord[commandWord.length / 2] = Character.toUpperCase(commandWord[commandWord.length / 2]);
+        String middleCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(middleCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: all character is uppercase */
+        String allCharUppercaseCommand = ListCommand.COMMAND_WORD.toUpperCase();
+        assertTrue(parser.parseCommand(allCharUppercaseCommand) instanceof ListCommand);
+    }
+    //@@author
 
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() throws Exception {
